6113efcd5a2f3744950e41f00a8ef9487d65c9c2
mathieu bernard <mathieu.bernard444@gmail.com>
2019-02-18 18:23:49 +0100

    controllers & views for island and user

---
+++ app/controllers/islands_controller.rb
@@ -0,0 +1,32 @@
+class IslandsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def index
+    @islands = Island.all
+  end
+
+  def new
+    @island = Island.new
+  end
+
+  def show
+    @island = Island.find(params[:id])
+  end
+
+  def create
+    @island = Island.new(island_params)
+    @island.user = current_user
+    if @island.save
+      redirect_to island_path(@island)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def island_params
+    params.require(:island).permit(:name, :location, :capacity, :price)
+  end
+
+end
