54a51e5216a0e1a86bef5c5246c7053f7ff88343
mathieu bernard <mathieu.bernard444@gmail.com>
2019-02-18 16:49:11 +0100

    models user and island

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_18_145622) do
+ActiveRecord::Schema.define(version: 2019_02_18_153858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "islands", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.string "availability"
+    t.integer "capacity"
+    t.integer "price"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_islands_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +39,5 @@ ActiveRecord::Schema.define(version: 2019_02_18_145622) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "islands", "users"
 end
