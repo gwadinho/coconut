9b9b4e6e8875ccb6ecf47395ace62d683b256463
mathieu bernard <mathieu.bernard444@gmail.com>
2019-02-19 16:31:12 +0100

    re pull <= validation de bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -7,7 +7,7 @@ class Booking < ApplicationRecord
   validate :end_date_after_start_date?
 
   def end_date_after_start_date?
-    if self.ending_date < self.start_date
+    if self.ending_date <= self.start_date
       errors.add(:ending_date, "cannot be before start date ")
     end
   end
