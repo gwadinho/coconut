a08a6926a153ee9ded1d4215fd6469353f243c22
Romain Bertrand <bertrand_romain@live.fr>
2019-02-21 15:08:39 +0100

    search and markers done

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -11,7 +11,7 @@ const buildMap = () => {
 };
 
 const addMarkersToMap = (map, markers) => {
- if (markers.length > 1) {
+ if (markers.length >= 1) {
    markers.forEach((marker) => {
      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
      new mapboxgl.Marker()
@@ -31,7 +31,7 @@ const addMarkersToMap = (map, markers) => {
 
 const fitMapToMarkers = (map, markers) => {
  const bounds = new mapboxgl.LngLatBounds();
- if (markers.length > 1) {
+ if (markers.length >= 1) {
    markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
    map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
  } else {
